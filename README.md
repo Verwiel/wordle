c788ca00df6ce82074a8d0fbbdb9f20467c7903e
Drew Verwiel <verwiel.code@gmail.com>
2022-07-31 09:14:42 -0600

    added a basic readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # wordle
+
+Rebuilding Wordle (https://www.nytimes.com/games/wordle/index.html) with some added features like word definitions and the ability to play unlimited games in a day
+
+Accounts and analytics will come in a later version
--- client/src/pages/WordleLanding.js
+++ client/src/pages/WordleLanding.js
@@ -1,5 +1,19 @@
+import axios from 'axios'
 
 const WordleLanding = () => {
+
+  const fetchRandomWord = async () => {
+    try {
+      let res = await axios.get(`http://api.wordnik.com/v4/words.json/randomWord?api_key=${process.env.REACT_APP_WORDNIK_API_KEY}`)
+      console.log(res)
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  // fetchRandomWord()
+
+
   return (
     <main>
       <header>
