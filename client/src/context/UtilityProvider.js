698863795bbceeea07046e06e4ac1df40556da6d
Drew Verwiel <verwiel.code@gmail.com>
2022-10-14 14:45:11 -0600

    added utility provider

---
+++ client/src/context/UtilityProvider.js
@@ -0,0 +1,31 @@
+// Provider to handle utility fnctions such as modal toggles
+import { createContext, useState, useContext } from 'react'
+
+const utilityContext = createContext()
+
+export const useUtilityCtx = () => useContext(utilityContext)
+
+export const UtilityProvider = ({ children }) => {
+  const [modalOpen, setModalOpen] = useState(false)
+  const [modalName, setModalName] = useState('')
+
+  const toggleModal = (selectedModal, action) => {
+    if(action === 'open'){
+      setModalName(selectedModal)
+      setModalOpen(true)
+    } else if(action === 'close'){
+      setModalOpen(false)
+      setModalName('')
+    }
+  }
+
+  return (
+    <utilityContext.Provider value={{
+      modalOpen,
+      modalName,
+      toggleModal
+    }}>
+      {children}
+    </utilityContext.Provider>
+  )
+}
--- client/src/index.js
+++ client/src/index.js
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { WordleProvider } from './context/WordleProvider'
 import { AuthProvider } from './context/AuthProvider'
+import { UtilityProvider } from './context/UtilityProvider'
 import App from './App'
 import './styles/main.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <WordleProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </WordleProvider>
-    </AuthProvider>
+    <UtilityProvider>
+      <AuthProvider>
+        <WordleProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </WordleProvider>
+      </AuthProvider>
+    </UtilityProvider>
   </React.StrictMode>
 )
