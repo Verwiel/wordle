f511e20ecbda4cab8cc0b6153e2a0f50d24ea3a1
Drew Verwiel <verwiel.code@gmail.com>
2022-10-17 12:45:46 -0600

    main modal styling

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -3,11 +3,29 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
 import { useUtilityCtx } from "../context/UtilityProvider"
 import { useOutsideClick } from '../hooks/useOutsideClick'
+import HowToPlay from './HowToPlay'
+import Settings from './Settings'
+import Statistics from './Statistics'
 
 const Modal = () => {
   const { modalName, toggleModal } = useUtilityCtx()
   const modalRef = useRef()
   useOutsideClick(modalRef, () => toggleModal('close', ''))
+
+  let displayedModal = '' 
+  switch(modalName){
+    case 'help':
+      displayedModal = <HowToPlay />
+      break;
+    case 'settings':
+      displayedModal = <Settings />
+      break;
+    case 'stats':
+      displayedModal = <Statistics />
+      break;
+    default: 
+      break;
+  }
 
   return (
     <div className='modal'>
@@ -16,7 +34,7 @@ const Modal = () => {
           <FontAwesomeIcon icon={faXmark} />
         </button>
         <h2>Modal</h2> 
-        <p>{modalName}</p>
+        {displayedModal}
       </section>
     </div>
   )
