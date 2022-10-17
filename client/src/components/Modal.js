c97ad24599bac7fa68e1489746603ddd43af88b9
Drew Verwiel <verwiel.code@gmail.com>
2022-10-17 10:52:04 -0600

    implemented outside click hook for modals

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,12 +1,17 @@
+import { useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
 import { useUtilityCtx } from "../context/UtilityProvider"
+import { useOutsideClick } from '../hooks/useOutsideClick'
 
 const Modal = () => {
   const { modalName, toggleModal } = useUtilityCtx()
+  const modalRef = useRef()
+  useOutsideClick(modalRef, () => toggleModal('close', ''))
+
   return (
     <div className='modal'>
-      <section className='modal-body'>
+      <section className='modal-body' ref={modalRef}>
         <button onClick={() => toggleModal('close', '')} className='modal-close'>
           <FontAwesomeIcon icon={faXmark} />
         </button>
