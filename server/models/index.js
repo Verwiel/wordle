6be1e411dfd41570b88582847b65dc2d0e08fc6f
Drew Verwiel <verwiel.code@gmail.com>
2023-04-05 15:31:24 -0600

    adding ssl required

--- server/models/index.js
+++ server/models/index.js
@@ -5,12 +5,14 @@ const sequelize = new Sequelize(
   config.DB,
   config.USER,
   config.PASSWORD,
+  
   {
     host: config.HOST,
     dialect: config.dialect,
     logging: false,
     // idle_timeout: 7200
     operatorsAliases: false,
+    ssl: true,
     pool: {
       max: config.pool.max,
       min: config.pool.min,
