8f49a3a8c1ced64c2b258c1f46e536b8fdcf7e7c
Drew Verwiel <verwiel.code@gmail.com>
2023-10-16 11:58:49 -0600

    updated models

--- server/models/game.model.js
+++ server/models/game.model.js
@@ -1,8 +1,8 @@
 module.exports = (sequelize, Sequelize) => {
   const Game = sequelize.define("games", {
     id: {
-      type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV4,
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
       primaryKey: true
     },
     // win - loss
